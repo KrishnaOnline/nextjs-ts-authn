2904abe5f5795fc70b633169f34d2fe2d748459c
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-15 18:24:31 +0530

    User Signup

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,18 +1,21 @@
 import mongoose, { mongo } from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export async function dbConnect() {
     try {
+        console.log(process.env.MONGO_URI);
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
         connection.on('connected', () => {
-            console.log("MongoDB Connected");
+            console.log("MongoDB Connected Successfully");
         });
         connection.on('error', (err) => {
             console.log("MongoDB Connection Error: "+err);
             process.exit();
         });
     } catch(err) {
-        console.log("Soemthing Went Wrong, while Connecting to DB");
+        console.log("Something Went Wrong, while Connecting to DB");
         console.log(err);
     }
 }
